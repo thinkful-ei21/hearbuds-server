16608ea47b6d369ead834b37acfe0e8513fabb49
chagoy <angelochagoy@gmail.com>
2018-08-22 12:48:32 -0700

    querying the ticketmaster database and returning events

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 const passport = require('passport');
 const morgan = require('morgan');
-
+const axios = require('axios');
+const { TICKETMASTER_BASE_URL, TICKETMASTER_API_KEY } = require('./config')
 
 const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 const {router: userRouter} = require('./users/router');
@@ -24,24 +25,94 @@ type User {
 	email: String!
 }
 
+type Event {
+	name: String
+	type: String
+	id: String
+	url: String
+	images: [Image]
+	classifications: [Classification]
+	promoter: Promoter
+	promoters: [Promoter]
+	seatmap: Seatmap
+	_embedded: Venue
+	dates: Date
+}
+
+type Image {
+	ratio: String
+	url: String
+	width: String
+	height: String
+	fallback: Boolean
+}
+
+type Seatmap {
+	staticUrl: String
+}
+
+type Date {
+	start: StartTime
+	spanMultipleDays: Boolean
+}
+
+type StartTime {
+	localDate: String
+	localTime: String
+	dateTime: String
+	dateTBD: Boolean
+	dateTBA: Boolean
+	timeTBA: Boolean
+}
+
+type Classification {
+	primary: Boolean
+	family: Boolean
+	genre: Genre
+}
+
+type Genre {
+	id: String
+	name: String
+}
+
+type Promoter {
+	id: String
+	name: String
+	description: String
+}
+
+type Venue {
+	name: String
+	type: String
+	id: String
+	postalCode: String
+	timezone: String
+}
+
 type Query {
 	getUser(id: ID!): User
+	getEvents: [Event]
 }
 `);
 
 
 
-const getUser = (args)=>{
+const getUser = (args) => {
   console.log(args);
   return 'user';
 };
 
+const getEvents = (args) => {
+	return axios.get(`${TICKETMASTER_BASE_URL}events.json?size=10&apikey=${TICKETMASTER_API_KEY}`)
+				.then(response => response.data._embedded.events)
+}
+
 
 // The root provides the top-level API endpoints
 const resolvers = {
-  getUser: (args) => getUser(args) 
-
-
+  getUser: (args) => getUser(args),
+  getEvents: (args) => getEvents(args)
 };
 
 var app = express();
